ac8c6fcb665836d9d4088040b5f603963f4142e9
Fred Heinecke <fred.heinecke@yahoo.com>
2025-05-02 08:06:23 +0000

    Fix Scanner service crash when RabbitMQ queues are predeclared
    Signed-off-by: Fred Heinecke <fred.heinecke@yahoo.com>

--- scanner/scanner/publisher.py
+++ scanner/scanner/publisher.py
@@ -11,41 +11,36 @@ logger = getLogger(__name__)
 
 
 class Publisher(RabbitBase):
-	QUEUE_RESCAN = "scanner.rescan"
+    QUEUE_RESCAN = "scanner.rescan"
 
-	async def __aenter__(self):
-		await super().__aenter__()
-		self._queue = await self._channel.declare_queue(self.QUEUE_RESCAN)
-		return self
+    async def _publish(self, data: dict):
+        await self._channel.default_exchange.publish(
+            Message(json.dumps(data).encode()),
+            routing_key=self.QUEUE,
+        )
 
-	async def _publish(self, data: dict):
-		await self._channel.default_exchange.publish(
-			Message(json.dumps(data).encode()),
-			routing_key=self.QUEUE,
-		)
+    async def add(self, path: str):
+        await self._publish({"action": "scan", "path": path})
 
-	async def add(self, path: str):
-		await self._publish({"action": "scan", "path": path})
+    async def delete(self, path: str):
+        await self._publish({"action": "delete", "path": path})
 
-	async def delete(self, path: str):
-		await self._publish({"action": "delete", "path": path})
+    async def refresh(
+            self,
+            kind: Literal["collection", "show", "movie", "season", "episode"],
+            id: str,
+            **_kwargs,
+    ):
+        await self._publish({"action": "refresh", "kind": kind, "id": id})
 
-	async def refresh(
-		self,
-		kind: Literal["collection", "show", "movie", "season", "episode"],
-		id: str,
-		**_kwargs,
-	):
-		await self._publish({"action": "refresh", "kind": kind, "id": id})
+    async def listen(self, scan):
+        async def on_message(message: AbstractIncomingMessage):
+            try:
+                await scan()
+                await message.ack()
+            except Exception as e:
+                logger.exception("Unhandled error", exc_info=e)
+                await message.reject()
 
-	async def listen(self, scan):
-		async def on_message(message: AbstractIncomingMessage):
-			try:
-				await scan()
-				await message.ack()
-			except Exception as e:
-				logger.exception("Unhandled error", exc_info=e)
-				await message.reject()
-
-		await self._queue.consume(on_message)
-		await asyncio.Future()
+        await self._queue.consume(on_message)
+        await asyncio.Future()

solidDoWant/Kyoo

commit cf7bc456e8bcf1de17341c2f199d43590ade8435

Fix Scanner service crash when RabbitMQ queues are predeclared
Signed-off-by: Fred Heinecke <fred.heinecke@yahoo.com>

scanner/scanner/publisher.py
@@ -13,11 +13,6 @@ logger = getLogger(__name__)
 class Publisher(RabbitBase):
 	QUEUE_RESCAN = "scanner.rescan"
 
-	async def __aenter__(self):
-		await super().__aenter__()
-		self._queue = await self._channel.declare_queue(self.QUEUE_RESCAN)
-		return self
-
 	async def _publish(self, data: dict):
 		await self._channel.default_exchange.publish(
 			Message(json.dumps(data).encode()),

scanner/providers/rabbit_base.py
@@ -13,9 +13,22 @@ class RabbitBase:
 			login=os.environ.get("RABBITMQ_DEFAULT_USER", "guest"),
 			password=os.environ.get("RABBITMQ_DEFAULT_PASS", "guest"),
 		)
+
+		# Attempt to declare the queue passively in case it already exists.
+		try:
+			self._channel = await self._con.channel()
+			self._queue = await self._channel.declare_queue(self.QUEUE, passive=True)
+			return self
+		except Exception:
+			# The server will close the channel on error.
+			# Cleanup the reference to it.
+			await self._channel.close()
+
+		# The queue does not exist, so actively declare it.
 		self._channel = await self._con.channel()
 		self._queue = await self._channel.declare_queue(self.QUEUE)
 		return self
 
 	async def __aexit__(self, exc_type, exc_value, exc_tb):
+		await self._channel.close()
 		await self._con.close()

commit cac9714e1737aaa75e55b6ef077d19973409c232

Fix scanner message storm caused by #916
Signed-off-by: Fred Heinecke <fred.heinecke@yahoo.com>

scanner/scanner/__init__.py
@@ -6,12 +6,13 @@ async def main():
 	from .scanner import scan
 	from .refresher import refresh
 	from .publisher import Publisher
+	from .subscriber import Subscriber
 	from providers.kyoo_client import KyooClient
 
 	logging.basicConfig(level=logging.INFO)
 	logging.getLogger("watchfiles").setLevel(logging.WARNING)
 
-	async with Publisher() as publisher, KyooClient() as client:
+	async with Publisher() as publisher, Subscriber() as subscriber, KyooClient() as client:
 		path = os.environ.get("SCANNER_LIBRARY_ROOT", "/video")
 
 		async def scan_all():
@@ -21,5 +22,5 @@ async def main():
 			monitor(path, publisher, client),
 			scan_all(),
 			refresh(publisher, client),
-			publisher.listen(scan_all),
+			subscriber.listen(scan_all),
 		)

scanner/scanner/publisher.py
@@ -1,7 +1,5 @@
-import asyncio
 from guessit.jsonutils import json
 from aio_pika import Message
-from aio_pika.abc import AbstractIncomingMessage
 from logging import getLogger
 from typing import Literal
 
@@ -11,8 +9,6 @@ logger = getLogger(__name__)
 
 
 class Publisher(RabbitBase):
-	QUEUE_RESCAN = "scanner.rescan"
-
 	async def _publish(self, data: dict):
 		await self._channel.default_exchange.publish(
 			Message(json.dumps(data).encode()),
@@ -32,15 +28,3 @@ class Publisher(RabbitBase):
 		**_kwargs,
 	):
 		await self._publish({"action": "refresh", "kind": kind, "id": id})
-
-	async def listen(self, scan):
-		async def on_message(message: AbstractIncomingMessage):
-			try:
-				await scan()
-				await message.ack()
-			except Exception as e:
-				logger.exception("Unhandled error", exc_info=e)
-				await message.reject()
-
-		await self._queue.consume(on_message)
-		await asyncio.Future()

scanner/scanner/subscriber.py
@@ -1,0 +1,24 @@
+import asyncio
+from guessit.jsonutils import json
+from aio_pika.abc import AbstractIncomingMessage
+from logging import getLogger
+
+from providers.rabbit_base import RabbitBase
+
+logger = getLogger(__name__)
+
+
+class Subscriber(RabbitBase):
+	QUEUE = "scanner.rescan"
+
+	async def listen(self, scan):
+		async def on_message(message: AbstractIncomingMessage):
+			try:
+				await scan()
+				await message.ack()
+			except Exception as e:
+				logger.exception("Unhandled error", exc_info=e)
+				await message.reject()
+
+		await self._queue.consume(on_message)
+		await asyncio.Future()
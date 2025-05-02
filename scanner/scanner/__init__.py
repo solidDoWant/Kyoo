a70971c2ecc953d7d018161a561aa6d6ae1500e0
solidDoWant <fred.heinecke@yahoo.com>
2025-05-02 21:43:02 +0000

    attempted fix for message amp bug 3

--- scanner/scanner/__init__.py
+++ scanner/scanner/__init__.py
@@ -1,25 +1,26 @@
 async def main():
-    import asyncio
-    import os
-    import logging
-    from .monitor import monitor
-    from .scanner import scan
-    from .refresher import refresh
-    from .publisher import Publisher
-    from providers.kyoo_client import KyooClient
+	import asyncio
+	import os
+	import logging
+	from .monitor import monitor
+	from .scanner import scan
+	from .refresher import refresh
+	from .publisher import Publisher
+	from .subscriber import Subscriber
+	from providers.kyoo_client import KyooClient
 
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger("watchfiles").setLevel(logging.WARNING)
+	logging.basicConfig(level=logging.DEBUG)
+	logging.getLogger("watchfiles").setLevel(logging.WARNING)
 
-    async with Publisher() as publisher, KyooClient() as client:
-        path = os.environ.get("SCANNER_LIBRARY_ROOT", "/video")
+	async with Publisher() as publisher, Subscriber() as subscriber, KyooClient() as client:
+		path = os.environ.get("SCANNER_LIBRARY_ROOT", "/video")
 
-        async def scan_all():
-            await scan(path, publisher, client, remove_deleted=True)
+		async def scan_all():
+			await scan(path, publisher, client, remove_deleted=True)
 
-        await asyncio.gather(
-            monitor(path, publisher, client),
-            scan_all(),
-            refresh(publisher, client),
-            publisher.listen(scan_all),
-        )
+		await asyncio.gather(
+			monitor(path, publisher, client),
+			scan_all(),
+			refresh(publisher, client),
+			subscriber.listen(scan_all),
+		)
--- scanner/scanner/publisher.py
+++ scanner/scanner/publisher.py
@@ -1,7 +1,5 @@
-import asyncio
 from guessit.jsonutils import json
 from aio_pika import Message
-from aio_pika.abc import AbstractIncomingMessage
 from logging import getLogger
 from typing import Literal
 
@@ -11,60 +9,24 @@ logger = getLogger(__name__)
 
 
 class Publisher(RabbitBase):
-    QUEUE_RESCAN = "scanner.rescan"
+	async def _publish(self, data: dict):
+		logger.debug("Publishing %s", data)
+		await self._channel.default_exchange.publish(
+			Message(json.dumps(data).encode()),
+			routing_key=self.QUEUE,
+		)
+		logger.debug("Published %s", data)
 
-    async def __aenter__(self):
-        await super().__aenter__()
-        self._queue = await self._channel.declare_queue(self.QUEUE_RESCAN)
-        return self
+	async def add(self, path: str):
+		await self._publish({"action": "scan", "path": path})
 
-    async def _publish(self, data: dict):
-        logger.debug("Publishing %s", data)
-        await self._channel.default_exchange.publish(
-            Message(json.dumps(data).encode()),
-            routing_key=self.QUEUE,
-        )
-        logger.debug("Published %s", data)
+	async def delete(self, path: str):
+		await self._publish({"action": "delete", "path": path})
 
-    async def add(self, path: str):
-        await self._publish({"action": "scan", "path": path})
-
-    async def delete(self, path: str):
-        await self._publish({"action": "delete", "path": path})
-
-    async def refresh(
-            self,
-            kind: Literal["collection", "show", "movie", "season", "episode"],
-            id: str,
-            **_kwargs,
-    ):
-        await self._publish({"action": "refresh", "kind": kind, "id": id})
-
-    async def listen(self, scan):
-        async def on_message(message: AbstractIncomingMessage):
-            # logger.debug("Received message: %s", message.body)
-
-            data = json.loads(message.body)
-            logger.debug("Received message: %s", data)
-            # action = data.get("action", "")
-            # if action != "refresh":
-            #     logger.debug("Ignoring message: %s", message.body)
-            #     # Tell RabbitMQ to requeue the message, hopefully sending it to another consumer
-            #     # that can handle it.
-            #     # A better approach would be to use a separate queue for these messages, or
-            #     # routing keys. However, because rabbitmq will be removed in v5, this is probably
-            #     # not worth changing now.
-            #     # Warning: this can cause an infinite loop if the message if no consumers can handle
-            #     # it (usually only if the backend is unavailable).
-            #     await message.reject(requeue=True)
-            #     return
-
-            try:
-                await scan()
-                await message.ack()
-            except Exception as e:
-                logger.exception("Unhandled error", exc_info=e)
-                await message.reject()
-
-        await self._queue.consume(on_message)
-        await asyncio.Future()
+	async def refresh(
+		self,
+		kind: Literal["collection", "show", "movie", "season", "episode"],
+		id: str,
+		**_kwargs,
+	):
+		await self._publish({"action": "refresh", "kind": kind, "id": id})
